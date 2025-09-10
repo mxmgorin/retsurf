b0ff2c9fbc02582ed473abb7097a5f72fd2375ad
Troidem <onlinexmax@gmail.com>
2025-09-10 20:11:33 +0300

    feat: use web render

--- app/src/window.rs
+++ app/src/window.rs
@@ -1,13 +1,13 @@
-use crate::{config::InterfaceConfig, render::sdl2::Sdl2Renderer};
+use crate::{config::InterfaceConfig, render::AppRender};
 use sdl2::Sdl;
 
 pub struct AppWindow {
-    renderer: Sdl2Renderer,
+    renderer: AppRender,
 }
 
 impl AppWindow {
     pub fn new(sdl: &Sdl, config: &InterfaceConfig) -> Result<Self, String> {
-        let renderer = Sdl2Renderer::new(sdl, config);
+        let renderer = AppRender::new(sdl, config);
         Ok(Self { renderer })
     }
 
